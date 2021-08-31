9e42e3ca7ad0b9a843403b86c0d2eb677ed7befd
nikkursin <nikkursin18@gmail.com>
2021-08-31 21:38:28 +0300

    Finished UI

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Widget w;
+    w.setWindowTitle("Login");
     w.setFixedSize(250, 220);
+    w.setWindowIcon(QIcon("app_icon.ico"));
     w.show();
     return a.exec();
 }
